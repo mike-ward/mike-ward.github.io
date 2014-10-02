b684bd0df0be5acbe688b8b97b6793a4020b4f8a
mike-ward <mike@mike-ward.net>
2014-10-02 19:02:58 -0400

    launch editor after creating file

--- _scripts/new-post.js
+++ _scripts/new-post.js
@@ -1,35 +1,52 @@
 var fs = require('fs');
 var sys = require('sys');
 var slug = require('slug');
-var moment = require('moment')
+var moment = require('moment');
 
-function postFile (title) {
+function getTitle(input) {
+  var title = input.toString().trim();
+  if (title.length < 5) {
+    console.error('error: title less than 5 characters');
+    process.exit(1);
+  }
+  return title;
+}
+
+function buildFileName(title) {
   var name = moment().format('YYYY-MM-DD-') + slug(title) + '.md';
-  return name;
+  return name.toLowerCase();
+}
+
+function buildFrontMatter(title) {
+  var fm = [];
+  fm.push('---');
+  fm.push('layout: post');
+  fm.push('title:  "' + title + '"');
+  fm.push('---');
+  return fm.join('\n');
+}
+
+function writeFile(file, frontMatter) {
+  var path = '../_posts/' + file;
+  var err = fs.writeFileSync(path, frontMatter);
+  if (err) throw err;
+  console.log(path + ' created');
+  return path;
+}
+
+function launchEditor(path) {
+  require('child_process').exec('atom ' + path, function () {
+    process.exit(0);
+  });
 }
 
 var stdin = process.openStdin();
 console.log('Enter post title');
+
 stdin.addListener("data", function(d) {
-  var title = d.toString().trim();
-  if (title.length < 5) {
-    console.error('error: title less than 5 characters');
-    process.exit();
-  }
-  var file = postFile(title);
-  console.log('Post file:  ' + file);
-
-  var content = [];
-  content.push('---');
-  content.push('layout: post');
-  content.push('title:  "' + title + '"');
-  content.push('---')
-
-  var frontMatter = content.join('\n');
-  console.log(frontMatter);
-
-  var err = fs.writeFileSync('../_posts/' + file, frontMatter);
-  if (err) throw err;
-  console.log(file + ' created');
-  process.exit();
+  var title = getTitle(d);
+  var file = buildFileName(title);
+  var frontMatter = buildFrontMatter(title);
+  var path = writeFile(file, frontMatter);
+  launchEditor(path);
 });
